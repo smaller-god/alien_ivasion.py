a479f43f4bd4300a6f224ccd0c838969ec38bc27
smaller-god <smallergod451@gmail.com>
2025-02-10 17:39:24 +0300

    Добавил маски на пули и на корабли в Alien

---
+++ alien.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(pygame.sprite.Sprite):
+    """Класс, представляющий одного пришельца"""
+
+    def __init__(self, ai_game):
+        """Инициализирует пришельца и задаёт его начальную позицию"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Загрузка изображения пришельца и назначение атрибута rect
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # Каждый новый пришелец появляется в левом верхнем углу экрана
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Сохранение точной горизонтальной позиции пришельца
+        self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Возвращает True, если пришелец находится у края экрана"""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <=0)
+
+
+    def update(self):
+        """Перемещает пришельцев влево или вправо"""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
