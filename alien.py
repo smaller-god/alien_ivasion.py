1caf3e88a86fbf38efcc0565c847aea8afcea97f
smaller-god <smallergod451@gmail.com>
2025-02-13 17:26:28 +0300

    Сменил цвет пуль, было не видно

--- alien.py
+++ alien.py
@@ -11,7 +11,7 @@ class Alien(pygame.sprite.Sprite):
         self.settings = ai_game.settings
 
         # Загрузка изображения пришельца и назначение атрибута rect
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('images/alien.png')
         self.rect = self.image.get_rect()
 
         self.mask = pygame.mask.from_surface(self.image)
